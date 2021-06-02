f3120ee58fba8d1fb0e2319f4db494117c4b257e
priyanka610 <84711780+priyanka610@users.noreply.github.com>
2021-06-02 18:10:20 +0530

    commit

--- customer/src/main/java/com/hcl/wm/customer/CustomerApplication.java
+++ customer/src/main/java/com/hcl/wm/customer/CustomerApplication.java
@@ -1,13 +1,16 @@
 package com.hcl.wm.customer;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Customer API", version = "2.0", description = "Customers Information"))
 public class CustomerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CustomerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerApplication.class, args);
+    }
 
 }
